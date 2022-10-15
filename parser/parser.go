72d310e48345f890ed60756fce4456d50930a1ce
Michael DiBernardo <mikedebo@gmail.com>
2022-10-15 16:57:11 -0400

    Identifiers work.

--- parser/parser.go
+++ parser/parser.go
@@ -26,5 +26,46 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{LetToken: p.curToken}
+
+	if p.peekToken.Type != token.IDENTIFIER {
+		return nil
+	}
+	p.nextToken()
+
+	if p.peekToken.Type != token.ASSIGN {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{IdentToken: p.curToken, Value: p.curToken.Literal}
+
+	// Skip expression for now.
+	for p.curToken.Type != token.SEMICOLON {
+		p.nextToken()
+	}
+
+	return stmt
 }
